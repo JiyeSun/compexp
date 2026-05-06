1a0eb1a871574cd19f3a20c4b05aca4616659af7
JiyeSun <jiyesun217@gmail.com>
2026-05-06 12:39:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const questions = [
 ];
 
 const competitiveQuestions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const wrongAIQuestions = [3, 7, 9];
+const wrongAIQuestions = [3, 5, 7, 9];
 const QUESTION_TIME_LIMIT = 90;
 const aiTaunts = ["I got that one.", "You missed it!", "One step ahead.", "That was obvious.", "Mine."];
 const aiEncouragements = ["Next one’s mine.", "Watch me next!", "I’m not letting you win that easily.", "Just wait!", "I’m winning next."];
